982254176c54bf0867057f73ef236d85d0d5d882
K <kcbaumga315@gmail.com>
2022-10-09 22:09:33 -0400

    Progress w/ Parsing csv

--- CSharp/mlmodelcsharp/mlmodelcsharp/Program.cs
+++ CSharp/mlmodelcsharp/mlmodelcsharp/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using Microsoft.VisualBasic.FileIO;
+using Microsoft.VisualBasic;
 
 namespace mlmodelsharp
 {
@@ -18,6 +21,37 @@ namespace mlmodelsharp
             write.Write(buffer);
             stream.Close();
             Console.WriteLine("Completed");
+            
+                
+
+            var path = @"C/home/kyle/Gitrepo/WorkingRepo/CSharp/mlmodelcsharp/mlmodelcsharp/bin/Debug/calihousing.csv"; 
+            using (TextFieldParser csvParser = new TextFieldParser(path))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                csvParser.ReadLine();
+
+                while (!csvParser.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    string[] fields = csvParser.ReadFields();
+                    string longitude = fields[0];
+                    string latitude = fields[1];
+                    string housing_median_age = fields[2];
+                    string total_rooms = fields[3];
+                    string total_bedrooms = fields[4];
+                    string population = fields[5];
+                    string households = fields[6];
+                    string median_income = fields[7];
+                    string median_house_value = fields[8];
+                    string ocean_proximity = fields[9];
+
+                    Console.WriteLine(median_income);
+                }
+            }
         }
         
     }
